9f1f6687f4964c6f06f6cc8d55f20d64c6a28f5d
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-04-11 12:10:35 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,36 @@
+let antwoord_van_fibonaci = 0
+input.onButtonPressed(Button.A, function () {
+    antwoord_van_fibonaci = 0
+    basic.showNumber(144)
+    basic.showNumber(89)
+    basic.showNumber(55)
+    basic.showNumber(34)
+    basic.showNumber(21)
+    basic.showNumber(13)
+    basic.showNumber(8)
+    basic.showNumber(5)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showNumber(1)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    antwoord_van_fibonaci = 0
+    basic.showNumber(1)
+    basic.showNumber(1)
+    basic.showNumber(2)
+    basic.showNumber(3)
+    basic.showNumber(5)
+    basic.showNumber(8)
+    basic.showNumber(13)
+    basic.showNumber(21)
+    basic.showNumber(34)
+    basic.showNumber(55)
+    basic.showNumber(89)
+    basic.showNumber(144)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
